37353d14e714c5f5665013c69e7a47bafd85d91f
hanqiang <yin13753884368@163.com>
2017-03-30 14:18:36 +0800

    splash界面，登录，正则匹配，SharedPreference

---
+++ app/src/main/java/com/han/myframe/MainActivity.java
@@ -0,0 +1,15 @@
+package com.han.myframe;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.devilwwj.app.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
